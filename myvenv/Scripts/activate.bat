24e03f48ebdab8fbf4284041e08973b1113d4e1e
shayan <shanich03447@gmail.com>
2026-03-31 02:42:10 +0500

    start

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Internships\Medical-chatbot-with-guardrails\myvenv
+set VIRTUAL_ENV=D:\Internships\Medical-chatbot-with-guardrails\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
